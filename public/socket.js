66b4043f509100efc454480e967a0acc00c73e9c
Ahan <64554828+AhanR@users.noreply.github.com>
2021-05-31 13:35:49 +0530

    file management and phase selection
    client side files were reorganized to accommodate for the new files.
    
    the basic first phase goal of being able to collaboratively type in in action.
    
    Things that need to be implemented:
    >turn selection
    >backspace support
    >add interesting buttons
    >scale the website for mobile phones as well.

--- public/socket.js
+++ public/socket.js
@@ -1,14 +1,15 @@
 var socket = io.connect('http://localhost:3000');
-var userId,currentUser;
+var userId,currentUser,prevLength = 0;
 
 socket.on('connect', () => {
-    console.log("connecting to the interwebs server")
+    console.log("connected to the interwebs server")
 });
 
 //adding the message broadcasted from the server to messageBox
 socket.on('messageToClients',(data)=>{
     var messageBox = document.getElementById("messageBox");
-    messageBox.textContent += data.message;
+    messageBox.value += data.message;
+    prevLength = messageBox.value.length;
 
     //setting the controlling user (currentUser)
     currentUser = data.userName;
@@ -20,37 +21,37 @@ socket.on('messageToClients',(data)=>{
     console.log("message from server : "+data.message);
 });
 
+
+//getting user id / user name in the future
 socket.on('getUserId',(user)=>
 {
     userId = user;
 });
 
+
 //takikng the input and sending to the server
-function print(){
+function send(){
 
-    //send input box data to server
-    //data is the message from textbox
+
+    //send key up data to server
+    //data is the ey code of the key pressed
     //userData is the object that is being sent and broadcast
-    var data = document.getElementById("inputBox");
-    if(data.value != "")
+    var data = document.getElementById("messageBox").value;
+    if(data.length != prevLength && data != "")
     {    
-        //add newline here
-        data.value = data.value;
-
-        var userData = { userName : userId , message : data.value};
+        data = data.slice(-1);
+        var userData = { userName : userId , message : data };
         socket.emit('messgeToServer',userData);
-        console.log("me :"+data.value);
+        console.log("me :" + data);
+        prevLength = data.length;
     }
-     
+
     //adding message sent by user to messageBox
     var messageBox = document.getElementById("messageBox");
-    messageBox.textContent += (data.value);
+    messageBox.textContent += (data);
     currentUser = "me";
 
     //showing the controlling user (currentUser)
     var controllingUser = document.getElementById("controllingUser");
     controllingUser.textContent = "User in control : " + currentUser;
-
-    //clearing the input box
-    data.value = null;
 }
