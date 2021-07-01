ae558585fe273e22322a9374364d247f5f7467c9
Ahan <64554828+AhanR@users.noreply.github.com>
2021-07-02 01:47:26 +0530

    localhost:3000

--- public/socket.js
+++ public/socket.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost');
+var socket = io.connect('http://localhost:3000');
 // const socket = io("http://localhost", {
 //   withCredentials: true,
 //   extraHeaders: {
